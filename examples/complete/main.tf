da57a2061cc12d269762e5c9ee7c1335886fe48a
dkool <dkool@dkools-MacBook-Pro.local>
2024-03-08 09:37:58 +0100

    feat: add conditional expressions to allow some global properties and updated documentation

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -18,8 +18,9 @@ module "rg" {
 }
 
 module "kv" {
-  source  = "cloudnationhq/kv/azure"
-  version = "~> 0.1"
+  #source  = "cloudnationhq/kv/azure"
+  #version = "~> 0.1"
+  source = "../../"
 
   naming = local.naming
 
@@ -28,59 +29,15 @@ module "kv" {
     location      = module.rg.groups.demo.location
     resourcegroup = module.rg.groups.demo.name
 
-    keys = {
-      key1 = {
-        key_type = "RSA"
-        key_size = 2048
-        key_opts = [
-          "decrypt", "encrypt", "sign",
-          "unwrapKey", "verify", "wrapKey"
-        ]
-        rotation_policy = {
-          expire_after         = "P90D"
-          notify_before_expiry = "P30D"
-          automatic = {
-            time_after_creation = "P83D"
-            time_before_expiry  = "P30D"
-          }
-        }
-      }
-    }
-
-    secrets = {
-      random_string = {
-        secret1 = {
-          length  = 24
-          special = false
-        }
-      }
-      tls_keys = {
-        tls1 = {
-          algorithm = "RSA"
-          rsa_bits  = 2048
-        }
-      }
-    }
+    certs   = local.certs
+    keys    = local.keys
+    secrets = local.secrets
 
     issuers = {
       DigiCert = {
         org_id     = "12345"
         account_id = "12345"
         password   = "12345"
-      }
-    }
-
-    certs = {
-      cert1 = {
-        issuer             = "Self"
-        subject            = "CN=app1.demo.org"
-        validity_in_months = 12
-        exportable         = true
-        key_usage = [
-          "cRLSign", "dataEncipherment",
-          "digitalSignature", "keyAgreement",
-          "keyCertSign", "keyEncipherment"
-        ]
       }
     }
   }
