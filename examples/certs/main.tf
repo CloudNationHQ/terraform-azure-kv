a5c53b4d09c477bb937782499ce3428dc553569b
dkool <dkool@dkools-MacBook-Pro.local>
2024-12-04 11:47:08 +0100

    feat: change certificate usage

--- examples/certs/main.tf
+++ examples/certs/main.tf
@@ -18,8 +18,6 @@ module "rg" {
 }
 
 module "kv" {
-  #source  = "cloudnationhq/kv/azure"
-  #version = "~> 2.0"
   source = "../../"
 
   naming = local.naming
@@ -30,64 +28,38 @@ module "kv" {
     resource_group = module.rg.groups.demo.name
 
     certs = {
-      example = {
+      app1 = {
         issuer             = "Self"
         subject            = "CN=app1.demo.org"
         validity_in_months = 12
-        exportable         = true
+        key_type           = "RSA"
+        key_size           = "2048"
+        reuse_key          = false
+        content_type       = "application/x-pkcs12"
         key_usage = [
-          "cRLSign", "dataEncipherment",
-          "digitalSignature", "keyAgreement",
-          "keyCertSign", "keyEncipherment"
+          "cRLSign",
+          "dataEncipherment",
+          "digitalSignature",
+          "keyAgreement",
+          "keyCertSign",
+          "keyEncipherment"
         ]
+        extended_key_usage = [
+          "1.3.6.1.5.5.7.3.1",
+          "1.3.6.1.5.5.7.3.2"
+        ]
+        lifetime_actions = {
+          action1 = {
+            action_type        = "AutoRenew"
+            days_before_expiry = 30
+          }
+        }
+        subject_alternative_names = {
+          dns_names = ["app1.demo.org", "app1-dev.demo.org"]
+          emails    = ["admin@demo.org"]
+          upns      = ["user@demo.org"]
+        }
       }
     }
   }
 }
-
-#module "kv" {
-  #source = "../../"
-
-  #naming = local.naming
-
-  #vault = {
-    #name           = module.naming.key_vault.name_unique
-    #location       = module.rg.groups.demo.location
-    #resource_group = module.rg.groups.demo.name
-
-    #certs = {
-      #app1 = {
-        #issuer             = "Self"
-        #subject            = "CN=app1.demo.org"
-        #validity_in_months = 12
-        #key_type           = "RSA"
-        #key_size           = "2048"
-        #reuse_key          = false
-        #content_type       = "application/x-pkcs12"
-        #key_usage = [
-          #"cRLSign",
-          #"dataEncipherment",
-          #"digitalSignature",
-          #"keyAgreement",
-          #"keyCertSign",
-          #"keyEncipherment"
-        #]
-        #extended_key_usage = [
-          #"1.3.6.1.5.5.7.3.1",
-          #"1.3.6.1.5.5.7.3.2"
-        #]
-        #lifetime_actions = {
-          #action1 = {
-            #action_type        = "AutoRenew"
-            #days_before_expiry = 30
-          #}
-        #}
-        #subject_alternative_names = {
-          #dns_names = ["app1.demo.org", "app1-dev.demo.org"]
-          #emails    = ["admin@demo.org"]
-          #upns      = ["user@demo.org"]
-        #}
-      #}
-    #}
-  #}
-#}
